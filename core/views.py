44cecefbf2c2e69eccffd9a8fb40214428321f17
Matheus <mfabryny@minsait.com>
2022-07-06 10:50:27 -0300

    criado endpoint da lista

--- core/views.py
+++ core/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
 
-# Create your views here.
+from rest_framework import viewsets
+from rest_framework import permissions
+from .serializers import ListSerializer
+from .models import List
+
+class ListViewSet(viewsets.ModelViewSet):
+    queryset = List.objects.all()
+    serializer_class = ListSerializer
+    permission_classes = [permissions.IsAuthenticated]
