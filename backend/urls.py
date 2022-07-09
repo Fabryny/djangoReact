1b2c06cfd916a802924a1425699e0e17f42dc68c
Matheus <mfabryny@minsait.com>
2022-07-09 01:59:51 -0300

    Endpoint para pegar token por usuario

--- backend/urls.py
+++ backend/urls.py
@@ -20,6 +20,7 @@ from django.urls import include, path
 from rest_framework import routers
 from .views import UserViewSet, GroupViewSet
 from core.views import ListViewSet, ItemViewSet
+from rest_framework.authtoken import views
 
 router = routers.DefaultRouter()
 router.register(r'users', UserViewSet)
@@ -31,7 +32,7 @@ router.register(r'Item', ItemViewSet)
 # Additionally, we include login URLs for the browsable API.
 urlpatterns = [
     path('', include(router.urls)),
+    path('api-token-auth/', views.obtain_auth_token, name='api-token-auth'),
     path('admin/', admin.site.urls),
-    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
 
